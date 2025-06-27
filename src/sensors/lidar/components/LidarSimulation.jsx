0b7dda85bc2ae26be8e00e6a82c9a109fbaf0a07
tievenr <vineetraghu17@gmail.com>
2025-06-27 13:49:33 +0530

    added span

--- src/sensors/lidar/components/LidarSimulation.jsx
+++ src/sensors/lidar/components/LidarSimulation.jsx
@@ -4,6 +4,7 @@ import { OrbitControls, Stats } from '@react-three/drei';
 import Scene from '../../../components/Scene';
 import UIControls from '../../../components/UIControls';
 import { LidarConfigProvider } from '../context/LidarConfigContext';
+import * as THREE from 'three';
 
 const LidarSimulation = () =>
 {
@@ -13,14 +14,28 @@ const LidarSimulation = () =>
                 <Canvas camera={{ position: [ 0, 5, 15 ], fov: 50 }}>
                     <Scene />
                     <OrbitControls
-                        enableDamping={false}
-                        dampingFactor={0}
+                        // Smooth movement
+                        enableDamping={true}
+                        dampingFactor={0.05}
                         enablePan={true}
                         enableZoom={true}
                         enableRotate={true}
-                        maxPolarAngle={Math.PI}
+
+                        // Pan settings
+                        panSpeed={1.0}
+                        screenSpacePanning={false}  
+                        zoomSpeed={1.0}
                         minDistance={1}
                         maxDistance={100}
+                        maxPolarAngle={Math.PI}
+
+                        // Explicit mouse button mapping
+                        mouseButtons={{
+                            LEFT: THREE.MOUSE.ROTATE,
+                            MIDDLE: THREE.MOUSE.DOLLY,
+                            RIGHT: THREE.MOUSE.PAN
+                        }}
+
                     />
                     <Stats />
                 </Canvas>
