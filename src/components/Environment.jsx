fffa899fdf2bd140be0db135e88408d258026ebf
tievenr <vineetraghu17@gmail.com>
2025-07-23 13:56:45 +0530

    feat: added in roadref to bvh and set up box geometry instead of plane

--- src/components/Environment.jsx
+++ src/components/Environment.jsx
@@ -10,14 +10,18 @@ THREE.Mesh.prototype.raycast = acceleratedRaycast;
 
 const Environment = React.memo( () =>
 {
-    const sphereRef = useRef();
     const groundRef = useRef();
+    const roadRef = useRef();
 
     useEffect( () =>
     {
         if ( groundRef.current )
         {
             groundRef.current.geometry.computeBoundsTree();
+        }
+        if ( roadRef.current )
+        {
+            roadRef.current.geometry.computeBoundsTree();
         }
         // Add more refs here for other static meshes if needed
     }, [] );
@@ -35,6 +39,18 @@ const Environment = React.memo( () =>
                 <meshStandardMaterial color="#d4f0d4" roughness={0.8} metalness={0.1} />
             </mesh>
 
+            {/* Road as a box geometry */}
+            <mesh
+                ref={roadRef}
+                name="road"
+                position={[ 0, 0.05, 0 ]} // slightly above ground
+                receiveShadow
+                castShadow
+            >
+                {/* width, height (thickness), depth */}
+                <boxGeometry args={[ 32, 0.1, 800 ]} />
+                <meshStandardMaterial color="#c0c0c0" roughness={0.7} metalness={0.2} />
+            </mesh>
 
             {/* Static and dynamic instances */}
             <StaticInstances />
